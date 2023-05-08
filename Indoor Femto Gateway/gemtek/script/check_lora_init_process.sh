82fffd9f60b5296fb966e95b1c4597c930815020
Henrique Brandao <henrique.brandao@sagatechbrasil.com.br>
2023-05-08 10:33:09 -0300

    Alterando script para verifica o processo chirpstack-gateway-bridge

--- Indoor Femto Gateway/gemtek/script/check_lora_init_process.sh
+++ Indoor Femto Gateway/gemtek/script/check_lora_init_process.sh
@@ -1,13 +1,23 @@
-
 #!/bin/sh
 
 do_main() {
         now_lora=`ps w | grep -v grep | grep "lora_pkt_fwd"`
-        if [ "$now_lora" = "" ] ; then
-                
-		/etc/init.d/gtk_spilora.service0 start
+        now_gateway_brige=`ps w | grep -v grep | grep "chirpstack-gateway-bridge"`
+
+        echo $now_lora
+        echo $now_gateway_brige
+
+
+        if [ "$now_gateway_brige" = "" ] ; then
+                /etc/init.d/chirpstack-gateway-bridge start
         fi
+
+        if [ "$now_lora" = "" ] ; then
+                /etc/init.d/gtk_spilora.service0 start
+        fi
+
         return 0
 }
 
 do_main
+
